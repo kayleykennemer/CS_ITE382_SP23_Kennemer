8691c3a61a9a6998a6e556ed59afc82b5bb29a1e
kayleykennemer <93957160+kayleykennemer@users.noreply.github.com>
2023-04-02 21:07:20 -0500

    Add files via upload

--- LootLogger/LootLogger/ContentView.swift
+++ LootLogger/LootLogger/ContentView.swift
@@ -13,12 +13,13 @@ class ItemStore{
     var allItems = [Item]()
 }
 
-class Item {
-    var name: String
-    var valueInDollars: Int
-    var serialNumber: String?
-    let dateCreated: Date
-}
+class Item: Equatable{
+    static func ==(lhs: Item, rhs, rhs: Item)-> Bool{
+        return lhs.name == rhs.name
+        && lhs.serialNumber == rhs.serialNumber
+        && lhs.valueInDollars == rhs.valueInDollars
+        && lhs.dateCreated == rhs.dateCreated }
+    }
 
 init( name: String, serialNumber: String?, valueInDollars: Int){
     self.name = name
@@ -64,8 +65,17 @@ init (){
     }
 }
 
-override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-    return itemStore.allItems.count
+override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: Indexpath) {
+    if editingStyle == .delete{
+        let item = itemStore.allItems[indexpath.row]
+        itemStore.removeItem(item)
+        
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+    }
+}
+
+override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath){
+    itemStore.moveItem(from: sourceIndexPath.row, to: destinaitonIndexPath.row)
 }
 
 override func tableView(_ tableView: UITableView, cellForRowAt indexPath: indexPath) -> UITableViewCell{
@@ -76,4 +86,42 @@ override func tableView(_ tableView: UITableView, cellForRowAt indexPath: indexP
     cell.detailTextLabel?.text = "$\(item.valueInDollars)"
     
     return cell
+}
+
+@IBAction func addNewItem(_ sender: UIButton){
+    let newItem = itemStore.createItem()
+    
+    if let index = itemStore.allItems.firstIndex(of: newItem){
+        let indexPath = IndexPath(row: index, section: 0)
+    }
+    
+    tableView.insertRows(at: [indexPath], with: .automatic)
+}
+
+@IBAction func toggleEditingMode(_ sender: UIButton){
+    if isEditing{
+        sender.setTitle("Edit", for: .normal)
+        setEditing(false, animated:true)
+    }else{
+        sender.setTitle("Done", for: .normal)
+        setEditing(true, animated:true)
+    }
+}
+
+func removeItem(_ item: Item){
+    if let index = allItems.firstIndex(of: item){
+        allItems.remove(at: index)
+    }
+}
+
+func moveItem(from fromIndex: Int, to toIndex: Int){
+    if fromIndex == toIndex{
+        return
+    }
+    
+    let movedItem = allItems[fromIndex]
+    
+    allItems.remove(at: fromIndex)
+    
+    allItems.insert(movedItem, at: toIndex)
 }
